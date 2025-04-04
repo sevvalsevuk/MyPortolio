ef514e665c3500a8b4d089064292781b5e8df551
sevvalsevuk <sevvalsevuk@gmail.com>
2025-04-04 15:17:13 +0300

    Tema eklendi

--- MyPortfolio/DAL/Entities/Features.cs
+++ MyPortfolio/DAL/Entities/Features.cs
@@ -1,8 +1,10 @@
-﻿namespace MyPortolio.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPortolio.DAL.Entities
 {
   public class Features
   {
-
+    [Key]
     public int FeatureId { get; set; }
 
     public string Title { get; set; }
